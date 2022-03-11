135f9662b2a08318ed1eeecc95a596e11d744317
Marcelo <marcelo.ungaretti@gmail.com>
2022-03-11 17:13:36 -0300

    correcao

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,12 +1,18 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
+  const [pageNumber, setPageNumber] = useState(0);
+
+  useEffect(()=> {}, [])
+
   axios.get(`${BASE_URL}/movies?size=12&page=0`)
     .then(res => {
+      // const data = res.data as MoviePage
       console.log(res.data);
     });
 
